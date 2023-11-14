0238546d8f4c9e50b5e300be96ab2309db53d37b
Moaz Mirza <moazmirza13@gmail.com>
2023-11-14 20:50:46 +0500

    .env

--- backend/server/src/app.module.ts
+++ backend/server/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONG_URI),
+    BooksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
