876bd3a1c081e67741ee66a5cb159ebe69133856
Moaz Mirza <moazmirza13@gmail.com>
2023-11-15 05:35:32 +0500

    addBook

--- frontend/web/app/components/Footer.js
+++ frontend/web/app/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="text-center p-7 bg-black text-lg font-sans font-semibold text-white ">
+    <div className="text-center p-8 bg-black text-lg font-sans font-semibold text-white ">
       Made By <span> Moaz-13</span>
     </div>
   );
