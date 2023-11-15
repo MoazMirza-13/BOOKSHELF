434e303f26adde9df91aa4e2b9b916e979ceb95d
Moaz Mirza <moazmirza13@gmail.com>
2023-11-15 12:45:15 +0500

    deploying error handling

--- backend/server/src/main.ts
+++ backend/server/src/main.ts
@@ -6,7 +6,14 @@ import * as express from 'express';
 async function bootstrap() {
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  app.enableCors();
+
+  app.enableCors({
+    origin: 'https://book-store-ashen-gamma.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT || 4000);
 }
+
 bootstrap();
