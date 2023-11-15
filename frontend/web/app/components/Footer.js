411c6cfee72bb54629ada374c30b2f3dff45abd1
Moaz Mirza <moazmirza13@gmail.com>
2023-11-15 07:00:49 +0500

    footer,favicon

--- frontend/web/app/components/Footer.js
+++ frontend/web/app/components/Footer.js
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="text-center p-8 bg-black text-lg font-sans font-semibold text-white ">
-      Made By <span> Moaz-13</span>
+    <div className="text-center p-9 bg-black text-lg font-sans font-semibold text-white ">
+      Made By{" "}
+      <a href="https://github.com/MoazMirza-13" target="_blank">
+        {" "}
+        Moaz-13
+      </a>
     </div>
   );
 }
