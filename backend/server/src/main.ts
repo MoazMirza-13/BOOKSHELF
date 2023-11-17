d57987583b680856c8f1a776679cd1437d0966d0
Moaz Mirza <moazmirza13@gmail.com>
2023-11-18 01:36:55 +0500

    vercel

--- backend/server/src/main.ts
+++ backend/server/src/main.ts
@@ -6,14 +6,7 @@ import * as express from 'express';
 async function bootstrap() {
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-
-  app.enableCors({
-    origin: 'https://book-store-lged6nq5t-moazmirza-13.vercel.app/',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-
+  app.enableCors();
   await app.listen(process.env.PORT || 4000);
 }
-
 bootstrap();
