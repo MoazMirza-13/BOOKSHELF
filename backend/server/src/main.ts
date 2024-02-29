6524442bf621c6181103ded388ccfeda0d24a6a8
Moaz Mirza <moazmirza13@gmail.com>
2024-02-29 19:25:51 +0500

    main.ts

--- backend/server/src/main.ts
+++ backend/server/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(json({ limit: '16mb' }));
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(3000);
 }
 bootstrap();
