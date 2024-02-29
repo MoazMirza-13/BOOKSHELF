3bc3a76ab39dd10b8ca68a398c6c09097dc8255c
Moaz Mirza <moazmirza13@gmail.com>
2024-02-29 23:14:25 +0500

    deploy bug fix

--- backend/server/src/main.ts
+++ backend/server/src/main.ts
@@ -4,8 +4,15 @@ import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://bookstore-web.vercel.app', 'http://localhost:3000'], // Add your frontend domain here
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
   app.use(json({ limit: '16mb' }));
   await app.listen(3000);
 }
+
 bootstrap();
