612fa0d3d9c4089f4f63df113d3e0fb0702c72b5
Moaz Mirza <moazmirza13@gmail.com>
2024-03-01 00:04:08 +0500

    .

--- backend/server/src/main.ts
+++ backend/server/src/main.ts
@@ -1,36 +1,18 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { json } from 'express';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   app.enableCors({
-//     origin: ['http://localhost:3001', 'http://localhost:3000'], // Add your frontend domain here
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     preflightContinue: false,
-//     optionsSuccessStatus: 204,
-//     credentials: true,
-//   });
-//   app.use(json({ limit: '16mb' }));
-//   await app.listen(3000);
-// }
-
-// bootstrap();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { json } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
-  // Alternatively, you can specify the CORS options manually:
-  // app.enableCors({
-  //   origin: ['http://localhost:3001', 'http://localhost:3000', 'https://bookstore-web.vercel.app'],
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   preflightContinue: false,
-  //   optionsSuccessStatus: 204,
-  //   credentials: true,
-  // });
+  //  allow requests from all origins for practicing curd operations
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
 
   app.use(json({ limit: '16mb' }));
 
