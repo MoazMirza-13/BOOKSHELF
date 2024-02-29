35761419ed4704a805e5d076b30bba0d0c056011
Moaz Mirza <moazmirza13@gmail.com>
2024-02-29 20:32:29 +0500

    .

--- backend/server/src/main.ts
+++ backend/server/src/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import { json } from 'express';
 
 async function bootstrap() {
-  const server = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  app.use(json({ limit: '16mb' }));
+  await app.listen(3000);
 }
 bootstrap();
